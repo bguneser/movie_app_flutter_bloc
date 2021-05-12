f6e201e62a196885bfacff762f54d4a8837dc19b
bguneser <birgunguneser@gmail.com>
2021-05-13 01:04:40 +0300

    movie_app_flutter_bloc

---
+++ android/app/src/main/kotlin/com/bguneser/movie_app_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bguneser.movie_app_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app_flutter/src/app.dart';
+
+void main() {
+  runApp(App());
+}
+
+
